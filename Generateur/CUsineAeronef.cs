ffe66f6b2c6c3fb48529161ebfc09e75e68e3d4a
Emile Robillard <1549002@etu.cegep-lanaudiere.qc.ca>
2018-11-26 12:20:28 -0500

    enum typeAvion CUsineAeroport

--- Generateur/CUsineAeronef.cs
+++ Generateur/CUsineAeronef.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Xml.Serialization;
 
 namespace Generateur
 {
+	public enum typeAvion { Passager,Cargo,Loisir,Secours,Citerne};
+
 	public class CUsineAeronef
 	{
 		public CUsineAeronef()
@@ -14,23 +15,23 @@ namespace Generateur
 
 		}
 
-		public CAeronef CreerAeronef(string Type,string Nom, int Vitesse, int Entretient, int Capacite, int Embarquement, int Debarquement, int Data)
+		public CAeronef CreerAeronef(typeAvion Type,string Nom, int Vitesse, int Entretient, int Capacite, int Embarquement, int Debarquement, int Data)
 		{
-			if (Type == "Passager")
+			if (Type == typeAvion.Passager)
 				return new CPassager(Nom, Vitesse, Entretient, Capacite, Embarquement, Debarquement, Data);
-			else if (Type == "Cargo")
+			else if (Type == typeAvion.Cargo)
 				return new CCargo(Nom, Vitesse, Entretient, Capacite, Embarquement, Debarquement, Data);
 
 			return null;
 		}
 
-		public CAeronef CreerAeronef(string Type, string Nom, int Vitesse, int Entretient, int Capacite)
+		public CAeronef CreerAeronef(typeAvion Type, string Nom, int Vitesse, int Entretient, int Capacite)
 		{
-			if (Type == "Loisir")
+			if (Type == typeAvion.Loisir)
 				return new CLoisir(Nom, Vitesse, Entretient, Capacite);
-			else if (Type == "Secours")
+			else if (Type == typeAvion.Secours)
 				return new CSecours(Nom, Vitesse, Entretient, Capacite);
-			else if (Type == "Citerne")
+			else if (Type == typeAvion.Citerne)
 				return new CCiterne(Nom, Vitesse, Entretient, Capacite);
 
 			return null;
