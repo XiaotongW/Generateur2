67182975c4058c31c01f47647bc387d419ced70f
James <minijames10@hotmail.com>
2018-11-26 15:51:45 -0500

    Scenario

--- Generateur/Scenario.cs
+++ Generateur/Scenario.cs
@@ -8,13 +8,30 @@ namespace Generateur
 {
     public class Scenario
     {
-        List<CAeronef> aeroports;
+        List<CAeroport> ListeAeroports;
 
         public Scenario()
         {
-
+            ListeAeroports = new List<CAeroport>();
         }
 
+        public void AjouterAeroport(CAeroport Aeroport)
+        {
+            ListeAeroports.Add(Aeroport);
+        }
+        public void RetirerAeroport(CAeroport Aeroport)
+        {
+            ListeAeroports.Remove(Aeroport);
+        }
+
+        public void AjouterAeronef(CAeroport Aeroport,CAeronef Aeronef)
+        {
+             Aeroport.AjouterAeronef(Aeronef);
+        }
+        public void RetirerAeronef(CAeroport Aeroport,CAeronef Aeronef)
+        {
+            Aeroport.RetierAeronef(Aeronef);
+        }
 
     }
 }
