c371c0abfa2161f5f3f617a59812be0861d71adb
Emile Robillard <1549002@etu.cegep-lanaudiere.qc.ca>
2018-11-26 16:07:56 -0500

    Horloge

--- Simulateur/CHorloge.cs
+++ Simulateur/CHorloge.cs
@@ -8,9 +8,79 @@ namespace Simulateur
 {
 	class CHorloge
 	{
+		DateTime Debut;
+		DateTime DateNow;
+		bool enPause;
+		int[] TimeBuff;
+		int TimeSpeed;
+
 		public CHorloge()
 		{
+			Debut = DateTime.Now;
+			DateNow = Debut;
+			TimeBuff = new int[3];
+			for (int i =0; i < TimeBuff.Count(); i++)
+			{
+				TimeBuff[i] = 0;
+			}
+			TimeSpeed = 1;
+			enPause = false;
+		}
 
+		public string DebutHorloge()
+		{
+			Debut = DateTime.Now;
+			enPause = false;
+			return this.ToString();
+		}
+
+		public void setPause()
+		{
+			enPause = true;
+			Debut = DateNow;
+		}
+
+		public void restart()
+		{
+			enPause = false;
+			DateNow = DateTime.Now;
+			Debut = DateNow;
+		}
+
+		public bool isDiff()
+		{
+			if (!enPause) DateNow = DateTime.Now;
+			return enPause && ((Debut.Second != DateNow.Second) || (Debut.Minute != DateNow.Minute) || (Debut.Hour != DateNow.Second));
+		}
+
+		public override string ToString()
+		{
+			ChangeHorloge();
+			return string.Format("{0} : {1} : {2}", TimeBuff[0], TimeBuff[1], TimeBuff[2]);
+		}
+
+		private void ChangeHorloge()
+		{
+			int buffMinutes = 0;
+			int buffHeure = 0;
+			if (!isDiff()) return;
+			TimeBuff[2] += DateNow.Second - Debut.Second;
+			if (TimeBuff[2] > 59){
+				buffMinutes = (int)(TimeBuff[0]/60);
+				TimeBuff[2] = TimeBuff[2]%60;
+				
+			}
+
+			TimeBuff[1] += (DateNow.Minute - Debut.Minute)+buffMinutes;
+			if (TimeBuff[1] > 59)
+			{
+				buffHeure = (int)(TimeBuff[1] / 60);
+				TimeBuff[1]= TimeBuff[1]%60;
+			}
+			TimeBuff[0] += DateNow.Hour - Debut.Hour + buffHeure;
+			
+			
+			Debut = DateNow;
 		}
 	}
 }
