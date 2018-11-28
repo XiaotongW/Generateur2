aca3baa3b93216564241714ae9f9b52384d69f16
Emile Robillard <1549002@etu.cegep-lanaudiere.qc.ca>
2018-11-28 11:57:06 -0500

    Etat

---
+++ Simulateur/CObservation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulateur
+{
+	class CObservation:CVol
+	{
+		public CObservation(EtatAeronef etat):base(etat)
+		{
+
+		}
+	}
+}
---
+++ Simulateur/CVol.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulateur
+{
+	class CVol:Etat
+	{
+		public CVol(EtatAeronef etat):base(etat)
+		{
+
+		}
+	}
+}
